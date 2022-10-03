28d250a70ab473b0d4f7a6033f9017efe238acd3
Elena <kallaur@bk.ru>
2022-10-02 21:13:00 -0400

    Max

---
+++ Example_006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a =1;
+int b =2;
+int c =8;
+int d =5;
+int e =12;
+
+int max = a;
+
+if(a>max) max=a;
+if(b>max) max=b;
+if(c>max) max=c;
+if(d>max) max=d;
+if(e>max) max=e;
+
+Console.Write("максимальное число = ");
+Console.WriteLine(max);
